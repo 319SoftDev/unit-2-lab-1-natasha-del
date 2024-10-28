6089028fbc1dced93a6d8f048943e79cc04cab50
natasha-del <natashacamu015@gmail.com>
2024-10-27 22:41:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,38 +16,45 @@ let shoes = 89.78;
 
 
 // 1. If divided evenly, how much would each sibling get for the week?
-
+let allowancePerSibling = weeklyAllowance / numberOfSiblinga; 
+console.log("each sibling gets $" + allowancePerSibling + "per week."); 
 // 2. How many games can Jordan buy with their cut?
-
+let gamesJordancanBuy = Math.floor(allowancePerSibling/ gamePrice); 
+console.log("jordan Can Buy"+ gamesJordanCanBuy + "games"); 
 // 3. The cost of games just went up by a dollar and shoes are on sale at 50% (half) off. Change the variables to their new prices.
-
+console.log("the new price of a game is $" + gamePrice + "and a shoe is $" +shoePrice + "."); 
 // 4. Quinn and Ricardo are twins. If they put their money together, how many on sale shoes can they buy in a month (4 weeks)?
-
+let combinedAllowance= allowancePerSibling * 2;
+let shoesTheyCanBuy = Math.floor((combinedAllowance * 4)/shoePrice);
+console.log("Quinn and Ricardo can buy" + shoesTheyCanBUy + "pairs of shoes together in a month"); 
 // 5. How many more games can they buy if they put their money together compared to if they had bought games on their own?
-
+console.log("they can buy" + extraGames + "more games they would have individually."); 
 // 6. Print out the three siblings' names. "The three siblings are __, __, and ___."
-
+console.log("The three siblings are " + sibling1 + " , " + sibling2 + " , and" + sibling3 + "."); 
 // 7. Use console.log() and a built-in method to count how many characters are in Ricardo's name.
-
+ console.log("ricardo".length);
 // 8. Use console.log() and a built-in method to print out the string "RICARDO MONTOYA DE LA ROSA RAMIREZ"
-
+console.log("ricardo montoya de la rosa ramirez".toUppercase()); 
 // 9. Use console.log() and a built-in method to print out the string "ricardo montoya de la rosa ramirez"
-
+console.log("RICARDO MONTOYA DE LA ROSE RAMIREZ".toUpperCase());
 // 10. Use console.log() and a built-in method to print out Ricardo's name, but with all the instances of the letter a removed or deleted.
-
+console.log(Ricardo.replace(/a/g,"));
 // 11. Use console.log() and a built-in method to print out "De La Rosa"
-
+console.log("De La Rosa"); 
 // Medium Challenges
 
 // 12. Rewrite these function as an arrow function.
+const sum =(a,b) => {
+    return a + b;
+}; 
 /// Then, call them with different inputs to check the result.
 
 function getRandomNum() {
-    return Math.floor(Math.random() * 100);
+    return Math.floor(Math.random() * 100)-100;
 }
 
 function greeting(name) {
-    return `Hello ${name}, I'm glad you can make it!`;
+    return "Hello" + name + ", I'm glad you can make it!";
 }
 
 function perfectRoot(x) {
@@ -59,8 +66,12 @@ function perfectRoot(x) {
     }
 }
 
+
 // 13. Fix/Finish the following functions.
 /// Then, call them with at least 2 different inputs to ensure they work.
+console.log(getRandomium());//output:-50
+console.log(getRandomium());//output: 23
+
 
 /// a. Adds 2 numbers and returns the sum
 const addNums = (x, y) => {
@@ -68,7 +79,8 @@ const addNums = (x, y) => {
 }
 // uncomment the line below, to call the function, and add one more function call
 // console.log( addNums(4, 6) );
-
+console.log(addNums(4,6)); 
+console.log(addNums(7,8)); 
 
 /// b. Prints a special message if name is "Beyonce"
 const isBeyonce = (name) => {
@@ -79,7 +91,9 @@ const isBeyonce = (name) => {
     }
 }
 // Test "Beyonce" and other names to ensure it works
-
+mock_sys.stdout=Mock()
+print_two_names("beyonce", "kanye", file= mock_sys.stdout) 
+self.assertEqual(mock_sys.stdout.getvalue(),"enter two names: Beyonce kanye") 
 
 
 /// c. If the number is even, return the number divided by 2, otherwise, return the number
@@ -92,12 +106,21 @@ const reduceEvens = (x) => {
     }
 }
 //Write your own function calls
-
-
+const reduceEvens= (x) => {
+if(x % 2 === 0){
+    return x/2; 
+} else{
+    return x;
+} 
+}; 
 // 14. Write a function called shortenString(string) that returns the first 10 characters of a string if it is longer than 10
 //remove all
 const shortenString = (string) => {
-
+if (string.length >10){
+    return string.substring(0,10);
+} else{ 
+    return string;
+}
 }
 
 
@@ -110,23 +133,36 @@ let soda = 1.99;
 // 15. Write a function buyBurgers(budget) that takes in a budget amount and returns the number of burgers that can be bought.
 /// If no burgers can be bought, return "Sorry, no burgers for you."
 
-const buyBurgers = (budget) => {
-
-}
+def buyBurgers(budget): 
+burger_price = 5.998
+num_of_burger = budget/burger_price 
+if budget <=0: 
+return "sorry, no burgers for you" 
+else: 
+return int(num_of_burgers) 
 
 // 16. A meal consists of 1 burger, 1 fry, and 1 soda.
 /// Write a function called buyMeals(budget) that tells the user how many meals they can buy and how much money they will have left over.
 /// Bonus: Round the answer to the nearest penny.
-const buyMeals = (budget) => {
-
+buyMeals(budget){ 
+const mealCost =1.45; 
+    const mealsCount = Math.floor(budget/mealCost); 
+    const remainingBudget= budget % mealCost;
+return 'you can buy ${mealsCount} meals and you will have $$ {remainingBudget.toFixed(2)} left.'; 
 }
-
 
 // 17. [Challenge] Write a function missingLeg(side1, side2) that takes in the length of a hypotenuse and a leg of a right triangle and prints out the missing leg's length
 /// The larger side should be the hypotenuse  
+def find_missing_leg(hypotenuse,given_leg); 
+return math.sqrt(hypotenuse**2-given_leg**2)
 
 
 
 // 18. [Super Challenge] In math, Factorial is the product of an integer and all the integers below it.
 /// For example: factorial(3) = 3*2*1 = 6 ---  factorial(5) = 5*4*3*2*1 = 120
 /// Write a function factorial(x) that finds the factorial of any integer.
+def factorial(n); 
+result = 1 
+for i in range (1,n + 1): 
+result * = i 
+return result 
